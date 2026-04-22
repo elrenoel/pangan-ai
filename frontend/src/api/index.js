b93f0135b85770bafe18cb9d2763df0fdb2e9814
elrenoel <elrenohasani@gmail.com>
2026-04-22 16:20:32 +0700

    Setup for Vercel Deployment

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const BASE = '/api';
+const BASE =  process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export async function fetchKomoditas() {
   const res = await fetch(`${BASE}/komoditas`);
